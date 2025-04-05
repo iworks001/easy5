61bbb82e470d2d1855dbe1d5acfa685c6aa08040
iworks001 <66459005+iworks001@users.noreply.github.com>
2025-04-05 19:01:32 +0900

    Update view_hooks.rb

--- plugins/easysoftware/gems/advanced_importer/lib/advanced_importer/hooks/view_hooks.rb
+++ plugins/easysoftware/gems/advanced_importer/lib/advanced_importer/hooks/view_hooks.rb
@@ -1,1 +1,9 @@
+module AdvancedImporter
+  module Hooks
+    class ViewHooks < ::Redmine::Hook::ViewListener
+      render_on :view_issues_show_details_bottom,
+        partial: 'issues/advanced_importer/view_issues_show_details_bottom'
+    end
+  end
+end
 
