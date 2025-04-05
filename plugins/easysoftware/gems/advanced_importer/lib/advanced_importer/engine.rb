ebea84c438733d650e0f01113adeabef88965f58
iworks001 <66459005+iworks001@users.noreply.github.com>
2025-04-05 18:51:01 +0900

    Update engine.rb

--- plugins/easysoftware/gems/advanced_importer/lib/advanced_importer/engine.rb
+++ plugins/easysoftware/gems/advanced_importer/lib/advanced_importer/engine.rb
@@ -10,5 +10,11 @@ module AdvancedImporter
       # Custom initializer
     end
 
+    # Rails 6+ 호환성 확보: module_parent 제공
+    unless respond_to?(:module_parent)
+      def self.module_parent
+        name.deconstantize.constantize
+      end
+    end
   end
 end
